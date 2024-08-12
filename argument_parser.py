06ce207bcd508ff7d44db52dc90b4cbc5c5591a2
djohoe28 <djohoe28@gmail.com>
2024-08-12 16:24:18 +0300

    argument_parser.py

---
+++ argument_parser.py
@@ -0,0 +1,29 @@
+import argparse
+from typing import Optional
+
+from argument_types import ParserArguments, Browsers, Formats
+
+
+def get_parser_arguments() -> ParserArguments:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--browser",
+                        choices=Browsers, default=Browsers[0],
+                        help="Browser for Selenium to use")
+    parser.add_argument("-u", "--url",
+                        default="https://gandalf.lakera.ai/gandalf-the-white",
+                        help="Base URL for Selenium to use")
+    parser.add_argument("-f", "--format",
+                        choices=Formats, default=Formats[0],
+                        help="Format of output file")
+    parser.add_argument("-o", "--output", type=Optional[str], help="Path to output file")
+    args = parser.parse_args()
+    return ParserArguments(**vars(args))
+
+
+def main():
+    args = get_parser_arguments()
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
