65f170226a86a52482637e77f2ed5987510ccaf2
djohoe28 <djohoe28@gmail.com>
2024-08-12 16:24:12 +0300

    argument_types.py

---
+++ argument_types.py
@@ -0,0 +1,29 @@
+from typing import Optional, Literal, List, Union, get_args
+from dataclasses import dataclass
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+
+BrowserType = Literal["auto", "firefox", "chrome", "edge", "safari"]
+FormatType = Literal["txt", "csv", "json"]
+Browsers: List[BrowserType] = list(get_args(BrowserType))
+Formats: List[FormatType] = list(get_args(FormatType))
+DriverType = Union[webdriver.Chrome, webdriver.Edge, webdriver.Firefox, webdriver.Safari]
+
+
+@dataclass
+class ParserArguments:
+    browser: BrowserType
+    url: str
+    format: FormatType
+    output: str
+
+
+@dataclass
+class Elements:
+    level_label: WebElement
+    description: WebElement
+    comment: WebElement
+    comment_submit: WebElement
+    answer: Optional[WebElement]
+    guess: Optional[WebElement]
+    guess_submit: Optional[WebElement]
