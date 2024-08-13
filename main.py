ecd991e7935bbcf2cc6f5ad6e3a6d4218af6597a
djohoe28 <djohoe28@gmail.com>
2024-08-13 16:37:04 +0300

    FIXED: - selenium_driver.py - - Added SeleniumDriver class - - Refactored `args` to `arguments` - - Added demo "submit" methods

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from argument_parser import get_parser_arguments
 
 def main():
     args = get_parser_arguments()
-    selenium_driver.main(args=args)
+    selenium_driver.main(arguments=args)
 
 
 if __name__ == '__main__':
--- selenium_driver.py
+++ selenium_driver.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 from argument_types import BrowserType, Browsers, DriverType, ParserArguments, Elements
 
@@ -17,6 +18,8 @@ def get_driver(browser: BrowserType = Browsers[0]) -> DriverType:
         case "safari":
             driver = webdriver.Safari()
         case "auto":
+            # TODO: Surround with try-catch ?
+            # Iterate through other drivers and find the first one that works.
             for browser in Browsers:
                 if browser == "auto":
                     continue
@@ -30,26 +33,63 @@ def get_driver(browser: BrowserType = Browsers[0]) -> DriverType:
     return driver
 
 
-def main(args: Optional[ParserArguments] = None):
-    print("Selenium Driver - Main")
-    driver = get_driver(args.browser)
-    driver.get(args.url)
-    # TODO: Refine CSS Selector - "#guess ~ button[type='submit']:first-of-type" ?
-    # comment_submit = driver.find_element(by=By.CSS_SELECTOR, value="button.h-7")
-    # guess_submit = driver.find_element(by=By.CSS_SELECTOR, value="button.button-white-to-gray-animation:nth-child(2)")
-    # comment_submit, guess_submit = driver.find_elements(by=By.CSS_SELECTOR, value="button[type='submit']")[:2]
-    elements = Elements(level_label=driver.find_element(by=By.CLASS_NAME, value="level-label"),
-                        description=driver.find_element(by=By.CLASS_NAME, value="description"),
-                        # TODO: Answer & Guess only appear after the first query has been sent...
-                        comment=driver.find_element(by=By.ID, value="comment"),
-                        comment_submit=driver.find_element(by=By.CSS_SELECTOR,
-                                                           value="button[type='submit']:nth-child(1)"),
-                        answer=None,  # driver.find_element(by=By.CLASS_NAME, value="answer"),
-                        guess=None,  # driver.find_element(by=By.ID, value="guess"),
-                        guess_submit=None  # driver.find_element(CSS_SELECT, value="button[type='submit']:nth-child(2)")
-                        )
-    print(elements.description)
-    driver.quit()
+class SeleniumDriver:
+    arguments: ParserArguments
+    driver: DriverType
+    elements: Elements
+
+    def __init__(self, arguments: ParserArguments):
+        self.arguments = arguments
+        self.driver = get_driver(self.arguments.browser)
+        self.get_elements()
+
+    def __del__(self):
+        self.driver.close()
+
+    def get_elements(self, is_queried: bool = False):
+        """
+        Updates the DOM :class:`WebElement` :py:attr:`elements`.
+
+        If the site hasn't been queried yet, the Answer & Guess elements will not be available, and thus skipped.
+
+        :param bool is_queried: Whether the site has been previously queried
+        """
+        self.driver.get(self.arguments.url)
+        # TODO: Refine CSS Selector - "#guess ~ button[type='submit']:first-of-type" ?
+        # comment_submit = driver.find_element(by=By.CSS_SELECTOR, value="button.h-7")
+        # guess_submit = driver.find_element(by=By.SELECT, value="button.button-white-to-gray-animation:nth-child(2)")
+        # comment_submit, guess_submit = driver.find_elements(by=By.CSS_SELECTOR, value="button[type='submit']")[:2]
+        self.elements = Elements(level_label=self.driver.find_element(by=By.CLASS_NAME, value="level-label"),
+                                 description=self.driver.find_element(by=By.CLASS_NAME, value="description"),
+                                 # TODO: Answer & Guess only appear after the first query has been sent...
+                                 comment=self.driver.find_element(by=By.ID, value="comment"),
+                                 comment_submit=self.driver.find_element(by=By.CSS_SELECTOR,
+                                                                         value="button[type='submit']:nth-child(1)"),
+                                 answer=self.driver.find_element(by=By.CLASS_NAME,
+                                                                 value="answer") if is_queried else None,
+                                 guess=self.driver.find_element(by=By.ID, value="guess") if is_queried else None,
+                                 guess_submit=self.driver.find_element(by=By.CSS_SELECTOR,
+                                                                       value="button[type='submit']:nth-child(2)"
+                                                                       ) if not is_queried else None
+                                 )
+
+    def submit_comment(self, value: str):
+        if len(value) < 10:
+            raise ValueError("Prompt must be at least 10 characters long.")
+        self.elements.comment.send_keys(value)
+        self.elements.comment_submit.click()
+        # TODO: Handle answer
+
+    def submit_guess(self, value: str):
+        if self.elements.answer is None:
+            raise BlockingIOError("You must submit a query before you can submit an answer!")
+        self.elements.guess.send_keys(value)
+        self.elements.guess_submit.click()
+        # TODO: Handle customAlert
+
+
+def main(arguments: Optional[ParserArguments] = None):
+    instance = SeleniumDriver(arguments)
 
 
 if __name__ == '__main__':
