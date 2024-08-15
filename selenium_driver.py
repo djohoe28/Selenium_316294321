287ce062e0c65da4e65a3ae3b3e54beb7d53e40f
djohoe28 <djohoe28@gmail.com>
2024-08-15 13:44:22 +0300

    Updated selenium_driver.py - Addressed "NoSuchDriverException" - Set all properties as private - Broke down `get_elements` - Refactored `submit` methods to return values

--- selenium_driver.py
+++ selenium_driver.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from selenium import webdriver
+from selenium.common import NoSuchDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
@@ -23,69 +24,88 @@ def get_driver(browser: BrowserType = Browsers[0]) -> DriverType:
             for browser in Browsers:
                 if browser == "auto":
                     continue
-                driver = get_driver(browser)
+                try:
+                    driver = get_driver(browser)
+                except NoSuchDriverException:
+                    print(f"No driver found for {browser.capitalize()}.")
                 if driver is not None:
+                    print(f"Driver found for {browser}.")
                     break
         case _:
-            # TODO: Fallback or Error?
-            # raise TypeError(f"Browser Type not supported: {arg}")
+            # TODO: raise TypeError(f"Browser Type not supported: {arg}")
+            print(f"Browser {browser} not recognized - falling back to auto.")
             return get_driver("auto")
     return driver
 
 
 class SeleniumDriver:
-    arguments: ParserArguments
-    driver: DriverType
-    elements: Elements
+    _arguments: ParserArguments
+    _driver: DriverType
+    _elements: Elements
+    _is_queried: bool
+    _is_submitted: bool
 
     def __init__(self, arguments: ParserArguments):
-        self.arguments = arguments
-        self.driver = get_driver(self.arguments.browser)
-        self.get_elements()
+        self._arguments = arguments
+        self._driver = get_driver(self._arguments.browser)
+        self._is_queried = False
+        self._is_submitted = False
+        self._elements = Elements(None, None, None, None, None, None, None)  # TODO: Make generic
+        self._get_all_elements()  # TODO: Extract?
 
     def __del__(self):
-        self.driver.close()
+        self._driver.close()
 
-    def get_elements(self, is_queried: bool = False):
+    def _get_all_elements(self):
         """
         Updates the DOM :class:`WebElement` :py:attr:`elements`.
 
         If the site hasn't been queried yet, the Answer & Guess elements will not be available, and thus skipped.
-
-        :param bool is_queried: Whether the site has been previously queried
         """
-        self.driver.get(self.arguments.url)
+        self._driver.get(self._arguments.url)
         # TODO: Refine CSS Selector - "#guess ~ button[type='submit']:first-of-type" ?
         # comment_submit = driver.find_element(by=By.CSS_SELECTOR, value="button.h-7")
         # guess_submit = driver.find_element(by=By.SELECT, value="button.button-white-to-gray-animation:nth-child(2)")
         # comment_submit, guess_submit = driver.find_elements(by=By.CSS_SELECTOR, value="button[type='submit']")[:2]
-        self.elements = Elements(level_label=self.driver.find_element(by=By.CLASS_NAME, value="level-label"),
-                                 description=self.driver.find_element(by=By.CLASS_NAME, value="description"),
-                                 # TODO: Answer & Guess only appear after the first query has been sent...
-                                 comment=self.driver.find_element(by=By.ID, value="comment"),
-                                 comment_submit=self.driver.find_element(by=By.CSS_SELECTOR,
-                                                                         value="button[type='submit']:nth-child(1)"),
-                                 answer=self.driver.find_element(by=By.CLASS_NAME,
-                                                                 value="answer") if is_queried else None,
-                                 guess=self.driver.find_element(by=By.ID, value="guess") if is_queried else None,
-                                 guess_submit=self.driver.find_element(by=By.CSS_SELECTOR,
-                                                                       value="button[type='submit']:nth-child(2)"
-                                                                       ) if not is_queried else None
-                                 )
+        self._get_default_elements()
+        if self._is_queried:
+            self._get_answer_elements()
+            self._get_guess_elements()
 
-    def submit_comment(self, value: str):
+    def submit_comment(self, value: str) -> str:
         if len(value) < 10:
             raise ValueError("Prompt must be at least 10 characters long.")
-        self.elements.comment.send_keys(value)
-        self.elements.comment_submit.click()
+        self._elements.comment.send_keys(value)
+        self._elements.comment_submit.click()
+        self._is_queried = True  # Wait?
         # TODO: Handle answer
+        return self._elements.answer.text  # TODO: response: [answer, success] ?
 
     def submit_guess(self, value: str):
-        if self.elements.answer is None:
+        if self._elements.answer is None:
             raise BlockingIOError("You must submit a query before you can submit an answer!")
-        self.elements.guess.send_keys(value)
-        self.elements.guess_submit.click()
+        self._elements.guess.send_keys(value)
+        self._elements.guess_submit.click()
         # TODO: Handle customAlert
+        return self._elements.answer.text  # TODO: response: [answer, success] ?
+
+    def _get_default_elements(self):
+        self._elements.level_label = self._driver.find_element(by=By.CLASS_NAME, value="level-label")
+        self._elements.description = self._driver.find_element(by=By.CLASS_NAME, value="description"),
+        self._elements.comment = self._driver.find_element(by=By.ID, value="comment"),
+        self._elements.comment_submit = self._driver.find_element(by=By.CSS_SELECTOR,
+                                                                  value="button[type='submit']:nth-child(1)"),
+
+    def _get_answer_elements(self):
+        # TODO: Answer & Guess only appear after the first query has been sent...
+        self._elements.answer = self._driver.find_element(by=By.CLASS_NAME,
+                                                          value="answer") if self._is_queried else None
+
+    def _get_guess_elements(self):
+        self._elements.guess = self._driver.find_element(by=By.ID, value="guess") if self._is_queried else None
+        self._elements.guess_submit = self._driver.find_element(by=By.CSS_SELECTOR,
+                                                                value="button[type='submit']:nth-child(2)"
+                                                                ) if self._is_queried else None
 
 
 def main(arguments: Optional[ParserArguments] = None):
