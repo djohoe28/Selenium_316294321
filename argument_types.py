049047595c372702a567f4c462948a38db6846c1
djohoe28 <djohoe28@gmail.com>
2024-08-16 13:38:39 +0300

    Added WebDriverWait under selenium_driver.py Added alert elements in argument_types.py Added some documentation

--- argument_types.py
+++ argument_types.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 
 BrowserType = Literal["auto", "firefox", "chrome", "edge", "safari"]
-FormatType = Literal["txt", "csv", "json"]
+FormatType = Literal["stdout", "txt", "csv", "json"]
 Browsers: List[BrowserType] = list(get_args(BrowserType))
 Formats: List[FormatType] = list(get_args(FormatType))
 DriverType = Union[webdriver.Chrome, webdriver.Edge, webdriver.Firefox, webdriver.Safari]
@@ -12,18 +12,37 @@ DriverType = Union[webdriver.Chrome, webdriver.Edge, webdriver.Firefox, webdrive
 
 @dataclass
 class ParserArguments:
+    """Data Class container for all available :class:`argparse.ArgumentParser` arguments"""
     browser: BrowserType
+    """Browser for Selenium to use"""
     url: str
+    """Base URL for Selenium to use"""
     format: FormatType
+    """Format of output file (stdout = print to standard output)"""
     output: str
+    """Path to output file"""
 
 
 @dataclass
 class Elements:
+    """Data Class container for all relevant :class:`WebElement` instances"""
     level_label: Optional[WebElement]
+    """The label displaying the current level"""
     description: Optional[WebElement]
+    """The description for the current level"""
     comment: Optional[WebElement]
+    """The textbox used to enter a comment (message) to the chatbot"""
     comment_submit: Optional[WebElement]
+    """The submit button used to submit the entered comment (message) to the chatbot"""
     answer: Optional[WebElement]
+    """The reply to the latest comment (message) to the chatbot (first appears after submitting a comment)"""
     guess: Optional[WebElement]
+    """The textbox used to enter a guess for the password answer (first appears after submitting a comment)"""
     guess_submit: Optional[WebElement]
+    """The textbox used to submit the entered password attempt (first appears after submitting a comment)"""
+    alert_title: Optional[WebElement]
+    """The header of the alert messagebox (appears after each password attempt submission)"""
+    alert_text: Optional[WebElement]
+    """The text content of the alert messagebox (appears after each password attempt submission)"""
+    alert_submit: Optional[WebElement]
+    """The button used to submit & close the alert messagebox (appears after each password attempt submission)"""
--- selenium_driver.py
+++ selenium_driver.py
@@ -1,8 +1,8 @@
 from typing import Optional
 from selenium import webdriver
-from selenium.common import NoSuchDriverException
+from selenium.common import NoSuchDriverException, NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
 
 from argument_types import BrowserType, Browsers, DriverType, ParserArguments, Elements
 
@@ -39,21 +39,33 @@ def get_driver(browser: BrowserType = Browsers[0]) -> DriverType:
 
 
 class SeleniumDriver:
+    """Class used to streamline handling the Selenium Driver"""
     _arguments: ParserArguments
+    """Parser Arguments received upon initialization"""
     _driver: DriverType
+    """The Selenium WebDriver used"""
     _elements: Elements
+    """The relevant :class:`WebElement` instances used"""
+    _wait: WebDriverWait
+    """Used to have the WebDriver wait for something to happen"""
     _is_queried: bool
-    _is_submitted: bool
+    """Used to indicate whether an initial comment (message) has been sent to the chatbot"""
+    _is_submitted: bool  # TODO: Unused?
+    """Used to indicate that a password guess has been submitted; Returns to False when customAlert is closed"""
 
     def __init__(self, arguments: ParserArguments):
+        """:class:`SeleniumDriver` Constructor - initializes all :py:attr:`_elements`"""
         self._arguments = arguments
         self._driver = get_driver(self._arguments.browser)
+        self._wait = WebDriverWait(self._driver, timeout=2, poll_frequency=.2,
+                                   ignored_exceptions=[NoSuchElementException, ElementNotInteractableException])
         self._is_queried = False
         self._is_submitted = False
-        self._elements = Elements(None, None, None, None, None, None, None)  # TODO: Make generic
+        self._elements = Elements(None, None, None, None, None, None, None, None, None, None)  # TODO: Make generic
         self._get_all_elements()  # TODO: Extract?
 
     def __del__(self):
+        """:class:`SeleniumDriver` Destructor - automatically closes the Selenium WebDriver instance."""
         self._driver.close()
 
     def _get_all_elements(self):
@@ -77,35 +89,65 @@ class SeleniumDriver:
             raise ValueError("Prompt must be at least 10 characters long.")
         self._elements.comment.send_keys(value)
         self._elements.comment_submit.click()
-        self._is_queried = True  # Wait?
-        # TODO: Handle answer
-        return self._elements.answer.text  # TODO: response: [answer, success] ?
+        self._is_queried = True
+        self._wait.until(lambda _: self._get_answer_elements())
+        if not self._has_answer_elements:
+            raise NoSuchElementException("Couldn't get answer elements.")
+        return self._elements.answer.text
 
-    def submit_guess(self, value: str):
-        if self._elements.answer is None:
+    def submit_guess(self, value: str) -> str:
+        if not self._has_guess_elements:
             raise BlockingIOError("You must submit a query before you can submit an answer!")
         self._elements.guess.send_keys(value)
         self._elements.guess_submit.click()
-        # TODO: Handle customAlert
-        return self._elements.answer.text  # TODO: response: [answer, success] ?
+        self._wait.until(lambda _: self._get_alert_elements())
+        if not self._has_alert_elements:
+            raise NoSuchElementException("Couldn't get customAlert elements.")
+        return f"{self._elements.alert_title.text}: {self._elements.alert_text.text}"  # TODO: Check if answer's correct
 
-    def _get_default_elements(self):
+    @property
+    def _has_default_elements(self) -> bool:
+        return None not in [self._elements.level_label, self._elements.description,
+                            self._elements.comment, self._elements.comment_submit]
+
+    def _get_default_elements(self) -> bool:
         self._elements.level_label = self._driver.find_element(by=By.CLASS_NAME, value="level-label")
-        self._elements.description = self._driver.find_element(by=By.CLASS_NAME, value="description"),
-        self._elements.comment = self._driver.find_element(by=By.ID, value="comment"),
+        self._elements.description = self._driver.find_element(by=By.CLASS_NAME, value="description")
+        self._elements.comment = self._driver.find_element(by=By.ID, value="comment")
         self._elements.comment_submit = self._driver.find_element(by=By.CSS_SELECTOR,
-                                                                  value="button[type='submit']:nth-child(1)"),
+                                                                  value="button[type='submit']:nth-child(1)")
+        return self._has_default_elements
 
-    def _get_answer_elements(self):
+    @property
+    def _has_answer_elements(self) -> bool:
+        return None not in [self._elements.answer]
+
+    def _get_answer_elements(self) -> bool:
         # TODO: Answer & Guess only appear after the first query has been sent...
         self._elements.answer = self._driver.find_element(by=By.CLASS_NAME,
                                                           value="answer") if self._is_queried else None
+        return self._has_answer_elements
 
-    def _get_guess_elements(self):
+    @property
+    def _has_guess_elements(self) -> bool:
+        return None not in [self._elements.guess, self._elements.guess_submit]
+
+    def _get_guess_elements(self) -> bool:
         self._elements.guess = self._driver.find_element(by=By.ID, value="guess") if self._is_queried else None
         self._elements.guess_submit = self._driver.find_element(by=By.CSS_SELECTOR,
                                                                 value="button[type='submit']:nth-child(2)"
                                                                 ) if self._is_queried else None
+        return self._has_guess_elements
+
+    @property
+    def _has_alert_elements(self) -> bool:
+        return None not in [self._elements.alert_title, self._elements.alert_text, self._elements.alert_submit]
+
+    def _get_alert_elements(self) -> bool:
+        self._elements.alert_title, self._elements.alert_text = self._driver.find_elements(
+            by=By.CSS_SELECTOR, value=".customAlert div:nth-child(2)")[:2]  # 1st & 2nd matches
+        self._elements.alert_submit = self._driver.find_element(by=By.CSS_SELECTOR, value=".customAlert button")
+        return self._has_alert_elements
 
 
 def main(arguments: Optional[ParserArguments] = None):
