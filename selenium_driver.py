128710974bffb20b20bb40b9a40b44754082e600
djohoe28 <djohoe28@gmail.com>
2024-08-16 14:01:52 +0300

    selenium_driver.py - level_label, cookies, STALE

--- selenium_driver.py
+++ selenium_driver.py
@@ -55,14 +55,16 @@ class SeleniumDriver:
 
     def __init__(self, arguments: ParserArguments):
         """:class:`SeleniumDriver` Constructor - initializes all :py:attr:`_elements`"""
-        self._arguments = arguments
+        self._arguments = arguments  # TODO: address url argument
         self._driver = get_driver(self._arguments.browser)
+        self._driver.delete_all_cookies()  # TODO: Parser Argument
         self._wait = WebDriverWait(self._driver, timeout=2, poll_frequency=.2,
                                    ignored_exceptions=[NoSuchElementException, ElementNotInteractableException])
         self._is_queried = False
         self._is_submitted = False
         self._elements = Elements(None, None, None, None, None, None, None, None, None, None)  # TODO: Make generic
         self._get_all_elements()  # TODO: Extract?
+        print(self._elements.level_label.text)  # TODO: Move?
 
     def __del__(self):
         """:class:`SeleniumDriver` Destructor - automatically closes the Selenium WebDriver instance."""
@@ -87,23 +89,35 @@ class SeleniumDriver:
     def submit_comment(self, value: str) -> str:
         if len(value) < 10:
             raise ValueError("Prompt must be at least 10 characters long.")
+        self._elements.comment.clear()
         self._elements.comment.send_keys(value)
         self._elements.comment_submit.click()
         self._is_queried = True
         self._wait.until(lambda _: self._get_answer_elements())
         if not self._has_answer_elements:
             raise NoSuchElementException("Couldn't get answer elements.")
+        self._wait.until(lambda _: self._get_guess_elements())
+        # TODO: Move?
+        if not self._has_guess_elements:
+            raise NoSuchElementException("Couldn't get guess elements")
+        print("(Psst! You can guess the answer now!)")
         return self._elements.answer.text
 
     def submit_guess(self, value: str) -> str:
         if not self._has_guess_elements:
             raise BlockingIOError("You must submit a query before you can submit an answer!")
+        self._elements.guess.clear()
         self._elements.guess.send_keys(value)
         self._elements.guess_submit.click()
         self._wait.until(lambda _: self._get_alert_elements())
         if not self._has_alert_elements:
             raise NoSuchElementException("Couldn't get customAlert elements.")
-        return f"{self._elements.alert_title.text}: {self._elements.alert_text.text}"  # TODO: Check if answer's correct
+        # TODO: Move?
+        answer = f"{self._elements.alert_title.text}: {self._elements.alert_text.text}"  # TODO: Check if answer correct
+        self._elements.alert_submit.click()
+        # print(self._elements.level_label.text)
+        # TODO: stale element (see bottom)
+        return answer
 
     @property
     def _has_default_elements(self) -> bool:
@@ -156,3 +170,21 @@ def main(arguments: Optional[ParserArguments] = None):
 
 if __name__ == '__main__':
     main()
+
+# TODO:
+""" 
+selenium.common.exceptions.StaleElementReferenceException: Message: The element with the reference daefa989-1570-416c-9be8-cf5ebfc6bdaf is stale; either its node document is not the active document, or it is no longer connected to the DOM; For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors#stale-element-reference-exception
+Stacktrace:
+RemoteError@chrome://remote/content/shared/RemoteError.sys.mjs:8:8
+WebDriverError@chrome://remote/content/shared/webdriver/Errors.sys.mjs:193:5
+StaleElementReferenceError@chrome://remote/content/shared/webdriver/Errors.sys.mjs:725:5
+getKnownElement@chrome://remote/content/marionette/json.sys.mjs:401:11
+deserializeJSON@chrome://remote/content/marionette/json.sys.mjs:259:20
+cloneObject@chrome://remote/content/marionette/json.sys.mjs:59:24
+deserializeJSON@chrome://remote/content/marionette/json.sys.mjs:289:16
+json.deserialize@chrome://remote/content/marionette/json.sys.mjs:293:10
+receiveMessage@chrome://remote/content/marionette/actors/MarionetteCommandsChild.sys.mjs:73:30
+
+
+Process finished with exit code 1
+"""
