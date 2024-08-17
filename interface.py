1b8065ffcab11d416e1c38ab8154f2e67d09ae68
djohoe28 <djohoe28@gmail.com>
2024-08-17 15:46:00 +0300

    Simplified interface.py, and added closure

--- interface.py
+++ interface.py
@@ -41,11 +41,10 @@ class Interface:
         """Query received via user input; Expecting any comment/guess string. Not used outside loop."""
         response: Any = None
         """The response from :class:`Controller` to the comment/guess. Not used outside loop."""
-        print("I am Gandalf, and Gandalf means me! (If you don't know what to do, enter 'help'!)")
-        # TODO: Indicate current Level to user (on entry / level up) -> make submits return an enum?
+        self._controller.print("I am Gandalf, and Gandalf means me! (If you don't know what to do, enter 'help'!)")
         while command.split(sep=" ", maxsplit=1)[0].lower() not in ["quit", "exit"]:
-            # Wait for user input
-            command = input("All we have to decide is what to do with the time that is given us: ")
+            # Wait for user input ("All we have to decide is what to do with the time that is given us: ")
+            command = input("> ")
             # Separate query from command (if found)
             if " " in command:
                 command, query = command.split(sep=" ", maxsplit=1)
@@ -54,12 +53,15 @@ class Interface:
             # Parse command
             match command.lower():
                 case "help":
-                    print(self.run.__doc__)
+                    self._controller.print(self.run.__doc__)
                 case "comment":
                     if query is None:
                         query = input("Please enter a comment to submit: ")
-                    response = self._controller.submit_comment(query)  # TODO: response: [answer, success] ?
-                    print(response)
+                    try:
+                        response = self._controller.submit_comment(query)
+                    except ValueError as ex:
+                        response = ex.args
+                    self._controller.print(response)
                 case "guess":
                     if query is None:
                         query = input("Please enter a guess to submit: ")
@@ -68,14 +70,15 @@ class Interface:
                     except NoSuchElementException as ex:
                         # ERROR: Can't submit a guess before submitting a comment for the level.
                         response = ex.msg
-                    print(response)
+                    self._controller.print(response)
                 case "exit":
                     break
                 case "quit":
                     break
                 case _:
-                    print("I have no memory of this place; Enter 'help' for a list of available commands.")
-        print("Farewell, my brave Hobbits. My work is now finished.")
+                    self._controller.print("I have no memory of this place; (Enter 'help' for a list of commands!)")
+        self._controller.print("Farewell, my brave Hobbits. My work is now finished.")
+        self._controller.close()
 
 
 def main(arguments: Optional[ParserArguments] = None):
